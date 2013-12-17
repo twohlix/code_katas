d10780f78fa1e3cf371fc4f9cebc58a02fef9f98
Charles Smith <charles@balconyinfive.com>
2013-12-17 03:28:50 -0500

    Halved memory usage by eliminating storing the words list, just went straight to anagram hash

--- 6_anagrams/anagram_generator.rb
+++ 6_anagrams/anagram_generator.rb
@@ -10,36 +10,40 @@ end
 def high_memory_version args
   usage_check args
   words_file = File.new(args[0], "r")
-
-  words_list = Array.new
-
-  while (line = words_file.gets)
-    word_candidate = line.strip
-    word_candidate.downcase!
-    words_list << word_candidate unless word_candidate.index("'") # get rid of possesives
-  end
-  
-  words_file.close
-
   anagrams = Hash.new
-  words_list.each do |word|
-    hash_key = word.strip.downcase.chars.sort.join
+
+  word_count = 0
+  while (line = words_file.gets)
+    word = line.strip
+    word.downcase!
+    next if word.index("'") # get rid of possesives
+    
+    hash_key = word.chars.sort.join
     anagrams[hash_key] = Array.new unless anagrams.has_key? hash_key
     anagrams[hash_key] << word
+
+    word_count += 1
   end
+  words_file.close
 
   output_file = File.new(args[1], "w")
+  anagram_sets_count = 0
   anagrams.each_value do |words|
+    next if words.length < 2
+
     output_line = ''
     words.each do |word|
       output_line << ' ' if output_line.length > 0 
       output_line << word
     end
     output_file.puts output_line
-  end
 
+    anagram_sets_count += 1
+  end
   output_file.close
-  ap "Done! #{words_list.length} words used."
+  
+  ap "Done! #{word_count} words used."
+  ap "#{anagram_sets_count} sets of anagrams found."
 end
 
 high_memory_version ARGV
--- 6_anagrams/notes.md
+++ 6_anagrams/notes.md
@@ -5,4 +5,4 @@ December 17, 2013 3am
 ---------------------
 So I've added a dict to ubuntu (apt-get install wamerican) to solve my first hurdle. I plan on reading in the words list into an array for possible words. Then to just do an O(n) search for anagrams I'll create a hash of sorted letters of the word being the key and the values being the words. So as I search through the word list, I just sort the string in alphabetical order and then I should just have to try and add a word for each sorted 'hash'. 
 
-What do you know. IT WORKED!
+What do you know. IT WORKED! Halved the memory usage by not even storing a words list to compare against. We can do it as we're reading it in. silly me.
