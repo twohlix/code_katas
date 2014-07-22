90580b90ae62dc3650b633ce705f472fac51ae60
Charles Smith <charles@balconyinfive.com>
2014-07-22 05:17:33 -0400

    Added sides_per_die argument

--- various_exercises/a.3.rb
+++ various_exercises/a.3.rb
@@ -1,16 +1,18 @@
 # roll the dice
-# take 1 arguments: number of dice to roll
+# take 1 or 2 arguments: number of dice to roll, [sides per die]
 def usage
-  puts "usage: #{$0} number_of_dice"
+  puts "usage: #{$0} number_of_dice [sides_per_die]"
   exit
 end
-usage unless ARGV.length == 1
+usage unless ARGV.length >= 1
 
 number_of_dice = ARGV[0].to_i
+sides_per_die = 6 #default sides per die
+sides_per_die = ARGV[1].to_i unless ARGV.length < 2
 
 (1..number_of_dice).each do |die_num|
-  result = 1 + rand(6)
+  result = 1 + rand(sides_per_die)
   print "#{result} "
 end
 
-print "(Probability: #{(1.0/6.0)**number_of_dice})\n"
+print "(Probability: #{(1.0/sides_per_die.to_f)**number_of_dice})\n"
